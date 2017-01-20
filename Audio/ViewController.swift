73c556948de2d8f98317613a3bc6c4f0c1186a54
anders617 <anders617@gmail.com>
2017-01-19 22:31:39 -0500

    Play/pause functionality

--- Audio/ViewController.swift
+++ Audio/ViewController.swift
@@ -7,19 +7,94 @@
 //
 
 import UIKit
+import AVFoundation
+
+extension TimeInterval {
+    /**
+     Returns string in format mm:ss interpreting this TimeInterval(Double) as seconds.
+    */
+    var timeString: String {
+        let currentMinutes = Int(self) / 60
+        let currentSeconds = Int(self.truncatingRemainder(dividingBy: 60))
+        return String(format: "%02d:%02d", currentMinutes, currentSeconds)
+    }
+}
 
 class ViewController: UIViewController {
 
+    @IBOutlet var currentTimeLabel: UILabel!
+    @IBOutlet var remainingTimeLabel: UILabel!
+    @IBOutlet var progressBar: UISlider!
+    
+    var progressUpdateTimer:Timer = Timer()
+    var audioPlayer: AVAudioPlayer!
+    var currentPlaybackRate:Float = 1.0 {
+        didSet {
+            audioPlayer.rate = currentPlaybackRate
+        }
+    }
+    
+    let fastForwardRate:Float = 2.0
+    let playRate:Float = 1.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let moonlight = NSDataAsset(name: "Moonlight")!
+        audioPlayer = try! AVAudioPlayer(data: moonlight.data)
+        audioPlayer.enableRate = true
+        audioPlayer.prepareToPlay()
+        
+        progressBar.minimumValue = 0
+        progressBar.maximumValue = Float(audioPlayer.duration)
+        
+        currentTimeLabel.text = (0.0).timeString
+        remainingTimeLabel.text = audioPlayer.duration.timeString
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func updateProgressBar() {
+        progressBar.value = Float(audioPlayer.duration * audioPlayer.currentTime / audioPlayer.duration)
+        
+        currentTimeLabel.text = audioPlayer.currentTime.timeString
+        remainingTimeLabel.text = (audioPlayer.duration - audioPlayer.currentTime).timeString
+    }
+    
+    func startTimer() {
+        progressUpdateTimer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(updateProgressBar) , userInfo: nil, repeats: true)
+    }
+    
+    func stopTimer() {
+        progressUpdateTimer.invalidate()
+    }
 
+    @IBAction func didPressPlay(_ sender: UIButton) {
+        currentPlaybackRate = playRate
+        audioPlayer.play()
+        startTimer()
+    }
 
+    @IBAction func didPressPause(_ sender: UIButton) {
+        audioPlayer.pause()
+        currentPlaybackRate = playRate
+        stopTimer()
+    }
+    
+    @IBAction func didPressFastForward(_ sender: UIButton) {
+        currentPlaybackRate = fastForwardRate
+        audioPlayer.play()
+    }
+    
+    @IBAction func didPressRewind(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func progressBarDidChange(_ sender: UISlider) {
+        audioPlayer.currentTime = TimeInterval(sender.value)
+    }
 }
 
